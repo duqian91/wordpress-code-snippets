792d6c29e7487e3002b36cf5ca0cc85191795025
duqian91 <du.qian91@gmail.com>
2016-05-03 22:56:59 -0400

    Update hide_default_roles.php

--- code/hide_default_roles.php
+++ code/hide_default_roles.php
@@ -1,7 +1,8 @@
 <?php
 
-function exclude_role($roles) {
+add_filter('editable_roles', 'exclude_role' );
 
+function exclude_role($roles) {
 //Hide Default Roles
   if (isset($roles['author'])) {
     unset($roles['author']);
@@ -22,5 +23,4 @@ function exclude_role($roles) {
   return $roles;
 }
 
-add_filter('editable_roles', 'exclude_role' );
 ?>
